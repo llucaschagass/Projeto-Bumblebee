169db246bb4957dda1a2011b06c3928bba451800
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2025-12-29 22:35:11 -0300

    feat: criando footer e ajustando espaçamentos

--- Desenvolvimento/Frontend/src/app/app.component.ts
+++ Desenvolvimento/Frontend/src/app/app.component.ts
@@ -2,18 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
+    CommonModule,
     RouterOutlet,
     HeaderComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+
 }
